d9e3dc47002b3bb6ccc3435ae7c66787ac63a59a
SHIPONmrs <107757338+SHIPONmrs@users.noreply.github.com>
2022-08-07 10:51:21 +0600

    Add files via upload

---
+++ Loading-Animation.py
@@ -0,0 +1,22 @@
+# Coded By Shipon_Mrs
+
+Name = input("What is your name?")
+Age = input("What is your age?")
+Number = input("What is your mobile number?")
+Pass =input("What is your pass?")
+
+print("Your name is","Your Age is",Number,Pass)
+
+#------------------[ LOGO-LAKNAT ]------------#
+def banner():
+	print(f'''\t{asu} ______                                              
+\t|_   _ \                                             
+$$\      $$\ $$$$$$$\   $$$$$$\  
+$$$\    $$$ |$$  __$$\ $$  __$$\ 
+$$$$\  $$$$ |$$ |  $$ |$$ /  \__|
+$$\$$\$$ $$ |$$$$$$$  |\$$$$$$\  
+$$ \$$$  $$ |$$  __$$<  \____$$\ 
+$$ |\$  /$$ |$$ |  $$ |$$\   $$ |
+$$ | \_/ $$ |$$ |  $$ |\$$$$$$  |
+\__|     \__|\__|  \__| \______/    	
+			{m}•{k}•{h}•{sir} Author : SHIPON {x}{m}•{k}•{h}•{x}''')
